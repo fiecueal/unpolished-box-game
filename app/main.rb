66a00928d8c291edaba768fc548ed17cc2f22e24
fiecueal <fiecueal@proton.me>
2023-11-19 09:28:12 -0800

    temporary collision check fix
    some quirks still exist with the collision checks for blocks that are
    next to each other

--- app/main.rb
+++ app/main.rb
@@ -17,19 +17,8 @@ def boot args = $args
     if @player.left > @player.platform.right ||
        @player.right < @player.platform.left ||
        @player.platform.hp && @player.platform.hp.zero?
-      new_block = args.
-                  geometry.
-                  find_intersect_rect([@player.x,
-                                       @player.y - 1,
-                                       @player.w,
-                                       @player.h],
-                                      @blocks)
-      if new_block
-        @player.platform = new_block
-      else
-        @player.platform = nil
-        @player.action   = @player_actions.freefall
-      end
+      @player.action   = @player_actions.freefall
+      @player.platform = nil
     end
 
     if args.inputs.up || args.inputs.keyboard.i
@@ -84,7 +73,7 @@ def boot args = $args
     if x.zero? || x == 19
       4.map { |y| new_block(64 * x, 64 * y) }
     else
-      new_block(64 * x, 0)
+      new_block(64 * x, x)
     end
   end.flatten!
 
@@ -96,56 +85,91 @@ def new_block x, y, hp = nil
   { x: x, y: y, w: 64, h: 64, path: "sprites/block_#{hp}.png", hp: hp }
 end
 
-# only handles collision when player is in motion
-def find_collision args
-  hit_block = args.geometry.find_intersect_rect(@player, @blocks)
-  return unless hit_block
-
-  if hit_block.hp
-    @blocks.delete hit_block if (hit_block.hp -= 1).zero?
-    hit_block.path = "sprites/block_#{hit_block.hp}.png"
-  end
-
-  # calc'd based on hit_block perspective
-  # always positive, hit direction is the smallest number
-  top_cl    = hit_block.top   - @player.bottom
-  right_cl  = hit_block.right - @player.left
-  bottom_cl = @player.top     - hit_block.bottom
-  left_cl   = @player.right   - hit_block.left
-
-  if top_cl < bottom_cl && top_cl < left_cl && top_cl < right_cl
-    # player lands on top of block
+def collide dir, hit_block
+  case dir
+  when :top
+    # player lands on top of hit_block instead of bouncing
     @player.y        = hit_block.top
     @player.dy       = 0
     @player.action   = @player_actions.grounded
     @player.platform = hit_block
     puts "\nhit top"
-  elsif bottom_cl < top_cl && bottom_cl < left_cl && bottom_cl < right_cl
-    @player.y  = hit_block.bottom - @player.h
-    @player.dy = -@player.dy
-    puts "\nhit bottom"
-  elsif right_cl < left_cl && right_cl < top_cl && right_cl < bottom_cl
+  when :right
     @player.x  = hit_block.right - @player.dx
     @player.dx = -@player.dx
     puts "\nhit right"
-  elsif left_cl < right_cl && left_cl < top_cl && left_cl < bottom_cl
+  when :bottom
+    @player.y  = hit_block.bottom - @player.h
+    @player.dy = -@player.dy
+    puts "\nhit bottom"
+  when :left
     @player.x  = hit_block.left - @player.w - @player.dx
     @player.dx = -@player.dx
     puts "\nhit left"
   end
+end
+
+# only handles collision when player is in motion
+def find_collision args
+  hit_blocks = args.geometry.find_all_intersect_rect(@player, @blocks)
+  return if hit_blocks.empty?
+
+  block = hit_blocks.first
+
+  hit_blocks.each do |b|
+    if b.hp
+      @blocks.delete b if (b.hp -= 1).zero?
+      b.path = "sprites/block_#{b.hp}.png"
+    end
+  end
+
+  # calc'd based on hit_block perspective
+  # always positive, hit direction is the smallest number
+  top_cl    = block.top     - @player.bottom
+  right_cl  = block.right   - @player.left
+  bottom_cl = @player.top   - block.bottom
+  left_cl   = @player.right - block.left
+
+  # makes things smoother when platforms are only slightly different in height
+  return collide :top, block if top_cl < 2 && @player.dy < 2
+
+  if @player.dy.to_i.zero? # if only moving horizontally
+    if    @player.dx.positive? then collide :left, block
+    elsif @player.dx.negative? then collide :right, block
+    end
+    puts "\ntop:#{top_cl}\nbottom:#{bottom_cl}\nright:#{right_cl}\nleft:#{left_cl}"
+    return
+  elsif @player.dx.to_i.zero? # if only moving vertically
+    if    @player.dy.positive? then collide :bottom, block
+    elsif @player.dy.negative? then collide :top, block
+    end
+    return
+  end
+
+  if top_cl < bottom_cl && top_cl < left_cl && top_cl < right_cl
+    collide :top, block
+  elsif bottom_cl < top_cl && bottom_cl < left_cl && bottom_cl < right_cl
+    collide :bottom, block
+  elsif right_cl < left_cl && right_cl < top_cl && right_cl < bottom_cl
+    collide :right, block
+  else
+    collide :left, block
+  end
   puts "\ntop:#{top_cl}\nbottom:#{bottom_cl}\nright:#{right_cl}\nleft:#{left_cl}"
+
+  find_collision args
 end
 
 def tick args
   timer         = args.easing.ease(@level_start_tick, args.state.tick_count, 1200, :identity)
   timer_flipped = 1 - timer
 
-  @player.action.call args
-  find_collision args
-
   @player.x     += @player.dx
   @player.y     += @player.dy
-  @player.angle -= @player.dx * 2
+  @player.angle -= @player.dx
+
+  @player.action.call args
+  find_collision args
 
   args.outputs.primitives << [
     @blocks,
